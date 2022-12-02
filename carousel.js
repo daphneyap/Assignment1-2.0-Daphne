b655fd8e2f28e153e91d562658aaf97c060ed0db
daphneyap <s10243748@connect.np.edu.sg>
2022-12-02 14:44:09 +0800

    draft

--- carousel.js
+++ carousel.js
@@ -1,12 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("topnav");
-    if (x.className === "navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "navigation";
-    }
-}
-
 let slideIndex = 0;
 showSlides();
 
